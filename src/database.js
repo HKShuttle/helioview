e991eaabb6617ed976f2d4f0a195e590eb608f7a
sola <terakou1105@gmail.com>
2025-06-01 23:25:39 +0900

    Organization, Bucketの作成処理を実装

--- src/database.js
+++ src/database.js
@@ -1,16 +1,15 @@
 import { InfluxDB } from '@influxdata/influxdb-client';
+import { OrgsAPI, BucketsAPI } from '@influxdata/influxdb-client-apis';
 import config from '../config.json' with { type: 'json' };
 
 export class Database {
   static #instance = null;
-
   #client;
 
   constructor() {
     if (Database.#instance) {
       throw new Error('Use Database.getInstance() instead of new.');
     }
-
     this.#client = new InfluxDB({
       url: config.influxdb.url,
       token: config.influxdb.token,
@@ -30,9 +29,65 @@ export class Database {
 
   async write(point) {
     this.#client.getWriteApi(config.influxdb.org, config.influxdb.bucket)
-    .writePoint(point);
+      .writePoint(point);
   }
 
-  // async createBucket(bucketName, orgId) { ... }
-  // async createOrganization(name) { ... }
+  async checkOrgs(orgName) {
+    const response = await new OrgsAPI(this.#client).getOrgs({ request: { org: orgName } });
+    const orgs = response.orgs ?? [];
+    const matched = orgs.find(o => o.name === orgName);
+    if (matched) {
+      return true;
+    }
+    return false;
+  }
+
+  async getOrgId(orgName) {
+    const response = await new OrgsAPI(this.#client).getOrgs({ request: { org: orgName } });
+    const orgs = response.orgs ?? [];
+    const matched = orgs.find(o => o.name === orgName);
+    if(matched){
+      return matched.id;
+    }
+    return null;
+  }
+
+  async checkBucket(bucketName) {
+    const response = await new BucketsAPI(this.#client).getBuckets();
+    const buckets = response.buckets ?? [];
+    const matched = buckets.find(b => b.name === bucketName);
+    if (matched) {
+      return true;
+    }
+    return false;
+  }
+
+  async createOrg(orgName) {
+    try {
+      await new OrgsAPI(this.#client).postOrgs({ body: { name: orgName } });
+    } catch (e) {
+      console.error(e);
+    }
+    return await this.checkOrgs(orgName);
+  }
+
+  async createBucket(bucketName, orgID) {
+    try {
+      await new BucketsAPI(this.#client).postBuckets({
+        body: {
+          name: bucketName,
+          orgID: orgID,
+          retentionRules: [{
+            type: "expire",
+            everySeconds: 0,
+            shardGroupDurationSeconds: 604800
+          }
+          ]
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    return await this.checkBucket(bucketName);
+  }
 }
